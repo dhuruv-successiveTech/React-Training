cb68026bdfd0ae1ab26c82c0e54eafec6d76a4ed
dhuruv kumar <dhuruv.kumar@successive.tech>
2025-06-19 15:21:29 +0530

    refactor : added components according to their questions

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
-export { default as Button } from './assignment-1/question-6/Button.jsx';
-export { default as Counter } from './assignment-1/question-4/Counter.jsx';
-export { default as Greeting } from './assignment-1/question-1/Greeting.jsx';
-export { default as TaskList } from './assignment-1/question-5/TaskList.jsx';
-export { default as UserCard } from './assignment-1/question-2/UserCard.jsx';
-export { default as Weather } from './assignment-1/question-3/Weather.jsx';
+export { default as Button } from './question-6/Button.jsx';
+export { default as Counter } from './question-4/Counter.jsx';
+export { default as Greeting } from './question-1/Greeting.jsx';
+export { default as TaskList } from './question-5/TaskList.jsx';
+export { default as UserCard } from './question-2/UserCard.jsx';
+export { default as Weather } from './question-3/Weather.jsx';
