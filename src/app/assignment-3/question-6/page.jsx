761628164af01784043562c799a8246604bea713
Dhruv Kumar <dhuruv.kumar@dhuruvvrs-31.successivetech.com>
2025-06-25 17:05:13 +0530

    feat : added question 6

---
+++ src/app/assignment-3/question-6/page.jsx
@@ -0,0 +1,17 @@
+const HomeAbout = () => {
+  return (
+    <>
+      <p className="question">
+        Q6. Create a list of items (e.g., products, articles) in your
+        application. Implement dynamic routing to display details for each item
+        when clicked. Create a route parameter that represents the item's ID in
+        the URL (e.g., "/products/:id"). Fetch item details based on the route
+        parameter and display them on the detail page. Add a "Go Back" button on
+        the detail page to return to the list.
+      </p>
+      <div>This is Dashboard</div>
+    </>
+  );
+};
+
+export default HomeAbout;
