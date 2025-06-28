a820e2a301454044c0bdf4cf4b05e0f8ae033659
Dhruv Kumar <dhuruv.kumar@dhuruvvrs-31.successivetech.com>
2025-06-28 18:51:12 +0530

    feat : added question 13 and 14 in assignemnt-4

--- src/app/assignment-4/question-13/page.jsx
+++ src/app/assignment-4/question-13/page.jsx
@@ -4,8 +4,12 @@ const FormPage = () => {
   return (
     <>
       <p className="question">
-        Q12. Build a data table using Material-UI's Table component. Populate
-        the table with sample data and add features like sorting and pagination.
+        Q13. install Yup. Define a Yup validation schema for a more complex form.
+        Include validation rules for fields like email, password, and phone
+        number. Ensure that error messages are displayed for each validation
+        rule. Implement real-time validation feedback using Material-UI's
+        TextField component. Show validation errors as the user types, and clear
+        the errors when the input is valid.(Use Formik as well)
       </p>
       <FormikComponent />
     </>
---
+++ src/app/assignment-4/question-14/page.jsx
@@ -0,0 +1,16 @@
+
+const FormPage = () => {
+  return (
+    <>
+      <p className="question">
+        Q14. Create a new form and Implement form submission handling. Ensure
+        that the form cannot be submitted if there are validation errors.
+        Display a summary of errors if the user attempts to submit an invalid
+        form.
+      </p>
+
+    </>
+  );
+};
+
+export default FormPage;
