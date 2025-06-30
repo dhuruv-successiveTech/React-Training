51fe4449470d47d0d3112b6ce708e517c808891f
Dhruv Kumar <dhuruv.kumar@dhuruvvrs-31.successivetech.com>
2025-06-30 11:58:52 +0530

    refactor : added action logic to different file

--- src/app/assignment-4/question-14/FormPage.jsx
+++ src/app/assignment-4/question-14/FormPage.jsx
@@ -2,6 +2,7 @@
 
 import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
+import { handleAction } from "./submitForm";
 
 const FormPage = () => {
   const [userName, setUserName] = useState("");
@@ -17,26 +18,7 @@ const FormPage = () => {
   const isFormValid =
     userName && password && confPassword && isPasswordValid && isPasswordMatch;
 
-  const handleAction = async (formData) => {
-    const username = formData.get("username");
-    const password = formData.get("password");
-    const confPassword = formData.get("confpassword");
 
-    if (!username || !password || !confPassword) {
-      return "All fields are required";
-    }
-    if (
-      password.length < 8 ||
-      !["*", "&", "!", "@", "#"].some((c) => password.includes(c))
-    ) {
-      return "Password must be at least 8 characters and contain special characters.";
-    }
-
-    if (password !== confPassword) {
-      return "Password do not match";
-    }
-    return "Form Submitted Successfully";
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
