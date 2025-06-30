7b9bf417c9f3e6165d3e2eb80107db178a23def2
Dhruv Kumar <dhuruv.kumar@dhuruvvrs-31.successivetech.com>
2025-06-30 11:55:45 +0530

    fix : removed unnecessary comments

--- src/app/assignment-5/question-6/UserData.jsx
+++ src/app/assignment-5/question-6/UserData.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import axios from "axios"; // ✅ Missing import added
+import axios from "axios";
 
 const UserData = ({ data }) => {
   const [user, setUser] = useState(data || []);
